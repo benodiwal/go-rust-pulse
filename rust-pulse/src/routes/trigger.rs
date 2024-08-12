efa637a3e0dac98ab7352121c3e48341ead85a3e
Sachin <s474996633@gmail.com>
2024-08-12 15:55:14 +0530

    rust trigger endpoint

--- rust-pulse/src/routes/trigger.rs
+++ rust-pulse/src/routes/trigger.rs
@@ -1,6 +1,21 @@
 use actix_web::{get, HttpResponse, Responder};
+use reqwest::Client;
 
 #[get("/trigger")]
 pub async fn trigger_go_server() -> impl Responder {
-    HttpResponse::Ok().body("Triggered Go Server")
+
+    let client = Client::new();
+    let url = "http://localhost:8000/trigger";
+
+    let result = client.get(url).send().await;
+    match result {
+        Ok(response) => {
+            if response.status().is_success() {
+                HttpResponse::Ok().body("Go Server Triggered Successfully")
+            } else {
+                HttpResponse::InternalServerError().body("Failed to Trigger Go Server")
+            }
+        },
+        Err(_) => HttpResponse::InternalServerError().body("Internal Server Error"),
+    }
 }
